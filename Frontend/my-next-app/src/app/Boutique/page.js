eb697712c3a1f1389fc7cb21df0105d7478801f8
littlekanine <elia.kopff3891@gmail.com>
2024-07-14 10:36:39 +0200

    add boutique page / start style

--- Frontend/my-next-app/src/app/Boutique/page.js
+++ Frontend/my-next-app/src/app/Boutique/page.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import styles from "./Boutique.module.scss";
+import Image from "next/image";
 
 const page = () => {
-  return (
-    <div>
-      <h1>New page</h1>
-    </div>
-  )
-}
+    return (
+        <div className={styles.boutiqueBackground}>
+            <div className={styles.boutique}>
+                <h1>Boutique</h1>
+                <section className={styles.articles}>
+                    <div className={styles.artcile}>
+                        <Image src={"/assets/images/articles/atelier_1.5.jpg"} width={100} height={100} />
+                    </div>
+                </section>
+            </div>
+        </div>
+    );
+};
 
-export default page
+export default page;
